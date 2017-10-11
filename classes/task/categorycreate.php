ad9c9b8e150a8a6f3c045378b042965715e3009b
roglos <roelmann@glos.ac.uk>
2017-10-11 09:13:51 +0100

    First Commit

---
+++ classes/task/categorycreate.php
@@ -0,0 +1,365 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * A scheduled task for scripted database integrations - category creation.
+ *
+ * @package    local_categorycreate - template
+ * @copyright  2016 ROelmann
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_categorycreate\task;
+use stdClass;
+use coursecat;
+
+defined('MOODLE_INTERNAL') || die;
+
+/**
+ * A scheduled task for scripted external database integrations.
+ *
+ * @copyright  2016 ROelmann
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class categorycreate extends \core\task\scheduled_task {
+
+    /**
+     * Get a descriptive name for this task (shown to admins).
+     *
+     * @return string
+     */
+    public function get_name() {
+        return get_string('pluginname', 'local_categorycreate');
+    }
+
+    /**
+     * Run sync.
+     */
+    public function execute() {
+
+        global $CFG, $DB;
+        require_once($CFG->libdir . "/coursecatlib.php");
+
+        // Check connection and label Db/Table in cron output for debugging if required.
+        if (!$this->get_config('dbtype')) {
+            echo 'Database not defined.<br>';
+            return 0;
+        } else {
+            echo 'Database: ' . $this->get_config('dbtype') . '<br>';
+        }
+        if (!$this->get_config('remotetable1')) {
+            echo 'Table1 not defined.<br>';
+            return 0;
+        } else {
+            echo 'Table: ' . $this->get_config('remotetable1') . '<br>';
+        }
+        if (!$this->get_config('remotetable2')) {
+            echo 'Table1 not defined.<br>';
+            return 0;
+        } else {
+            echo 'Table: ' . $this->get_config('remotetable2') . '<br>';
+        }
+
+        echo 'Starting connection...<br>';
+
+        // Report connection error if occurs.
+        if (!$extdb = $this->db_init()) {
+            echo 'Error while communicating with external database <br>';
+            return 1;
+        }
+
+        // EXTERNAL DB - TABLE1: Category levels.
+        // Get external table name.
+        $table = $this->get_config('remotetable1');
+        $levels = array();
+
+        // Read data from table1.
+        $sql = $this->db_get_sql($table, array(), array(), true);
+        if ($rs = $extdb->Execute($sql)) {
+            if (!$rs->EOF) {
+                while ($fields = $rs->FetchRow()) {
+                    $fields = array_change_key_case($fields, CASE_LOWER);
+                    $fields = $this->db_decode($fields);
+                    $levels[] = $fields;
+                }
+            }
+            $rs->Close();
+        } else {
+            // Report error if required.
+            $extdb->Close();
+            echo 'Error reading data from the external course table<br>';
+            return 4;
+        }
+
+        $cats = array();
+        foreach ($levels as $l) { // Loop through each level in turn to create a tree.
+            if ($l['inuse'] == 1) {
+
+                $level = $l['categorylevel'];
+
+                // EXTERNAL DB - TABLE2: Categories list.
+                // Get external table name.
+                $table2 = $this->get_config('remotetable2');
+
+                // Read data from table2.
+                $sql2 = $this->db_get_sql_like($table2, array("category_idnumber" => $level), array(), true);
+                if ($rs2 = $extdb->Execute($sql2)) {
+                    if (!$rs2->EOF) {
+                        while ($category = $rs2->FetchRow()) {
+                            $category = array_change_key_case($category, CASE_LOWER);
+                            $category = $this->db_decode($category);
+                            $cats[] = $category;
+
+                            // Create data to write category.
+                            $data = array();
+
+                            // ID number - UoG essential data!
+                            if (isset($category['category_idnumber'])) {
+                                $data['idnumber'] = $category['category_idnumber'];
+                            } else {
+                                echo 'Category IdNumber required';
+                                break;
+                            }
+
+                            // Name - If no name is set, make name = idnumber.
+                            if (isset($category['category_name']) && $category['category_name'] !== 'Undefined') {
+                                $data['name'] = $category['category_name'];
+                            } else {
+                                $data['name'] = $category['category_idnumber'];
+                            }
+
+                            // Default $parent values as Misc category, to give base values and get initial record.
+                            $parent = $DB->get_record('course_categories', array('name' => 'Miscellaneous'));
+                            $parent->id = 0;
+                            $parent->visible = 1;
+                            $parent->depth = 0;
+                            $parent->path = '';
+                            // If exists overide default $parent by fetching parent->id based on unique parent category idnumber.
+                            if (!$category['parent_cat_idnumber'] == '') {
+                                // Check if the parent category already exists - based on unique idnumber.
+                                if ($DB->record_exists('course_categories',
+                                    array('idnumber' => $category['parent_cat_idnumber']))) {
+                                    // Fetch that parent category details.
+                                    $parent = $DB->get_record('course_categories',
+                                    array('idnumber' => $category['parent_cat_idnumber']));
+                                }
+                            }
+                            // Set $data['parent'] as the id of the parent category and depth as parent +1.
+                            $data['parent'] = $parent->id;
+                            $data['depth'] = $parent->depth + 1;
+
+                            // Create a category that inherits visibility from parent.
+                            $data['visible'] = $parent->visible;
+                            // If a category is marked as 'deleted' then ensure it is hidden - don't actually delete it.
+                            if ($category['deleted']) {
+                                $data['visible'] = 0;
+                            }
+
+                            if (!$DB->record_exists('course_categories',
+                                array('idnumber' => $category['category_idnumber']))) {
+                                // Set new category id by inserting the data created above.
+                                coursecat::create($data);
+                                echo 'Category ' . $data['idnumber'] . ' added<br>';
+                            } else {
+                                // IF category already exists, fetch the existing id.
+                                $data['id'] = $DB->get_field('course_categories', 'id',
+                                    array('idnumber' => $category['category_idnumber']));
+                                // Set the path as necessary.
+                                $data['path'] = $parent->path . '/' . $data['id'];
+                                // As category already exists, update it with any changes.
+                                $DB->update_record('course_categories', $data);
+                                echo 'Category ' . $data['idnumber'] . ' updated<br>';
+                            }
+                        }
+                    }
+                    $rs2->Close();
+                } else {
+                    // Report error if required.
+                    $extdb->Close();
+                    echo 'Error reading data from the external course table<br>';
+                    return 4;
+                }
+            } else {
+                echo 'Category Level ' . $l['categorylevel'] . ' is not in use.<br>';
+            }
+        }
+        // Free memory.
+        $extdb->Close();
+    }
+
+
+
+
+
+    /* Db functions cloned from enrol/db plugin.
+     * ========================================= */
+
+    /**
+     * Tries to make connection to the external database.
+     *
+     * @return null|ADONewConnection
+     */
+    public function db_init() {
+        global $CFG;
+
+        require_once($CFG->libdir.'/adodb/adodb.inc.php');
+
+        // Connect to the external database (forcing new connection).
+        $extdb = ADONewConnection($this->get_config('dbtype'));
+        if ($this->get_config('debugdb')) {
+            $extdb->debug = true;
+            ob_start(); // Start output buffer to allow later use of the page headers.
+        }
+
+        // The dbtype my contain the new connection URL, so make sure we are not connected yet.
+        if (!$extdb->IsConnected()) {
+            $result = $extdb->Connect($this->get_config('dbhost'),
+                $this->get_config('dbuser'),
+                $this->get_config('dbpass'),
+                $this->get_config('dbname'), true);
+            if (!$result) {
+                return null;
+            }
+        }
+
+        $extdb->SetFetchMode(ADODB_FETCH_ASSOC);
+        if ($this->get_config('dbsetupsql')) {
+            $extdb->Execute($this->get_config('dbsetupsql'));
+        }
+        return $extdb;
+    }
+
+    public function db_addslashes($text) {
+        // Use custom made function for now - it is better to not rely on adodb or php defaults.
+        if ($this->get_config('dbsybasequoting')) {
+            $text = str_replace('\\', '\\\\', $text);
+            $text = str_replace(array('\'', '"', "\0"), array('\\\'', '\\"', '\\0'), $text);
+        } else {
+            $text = str_replace("'", "''", $text);
+        }
+        return $text;
+    }
+
+    public function db_encode($text) {
+        $dbenc = $this->get_config('dbencoding');
+        if (empty($dbenc) or $dbenc == 'utf-8') {
+            return $text;
+        }
+        if (is_array($text)) {
+            foreach ($text as $k => $value) {
+                $text[$k] = $this->db_encode($value);
+            }
+            return $text;
+        } else {
+            return core_text::convert($text, 'utf-8', $dbenc);
+        }
+    }
+
+    public function db_decode($text) {
+        $dbenc = $this->get_config('dbencoding');
+        if (empty($dbenc) or $dbenc == 'utf-8') {
+            return $text;
+        }
+        if (is_array($text)) {
+            foreach ($text as $k => $value) {
+                $text[$k] = $this->db_decode($value);
+            }
+            return $text;
+        } else {
+            return core_text::convert($text, $dbenc, 'utf-8');
+        }
+    }
+
+    public function db_get_sql($table, array $conditions, array $fields, $distinct = false, $sort = "") {
+        $fields = $fields ? implode(',', $fields) : "*";
+        $where = array();
+        if ($conditions) {
+            foreach ($conditions as $key => $value) {
+                $value = $this->db_encode($this->db_addslashes($value));
+
+                $where[] = "$key = '$value'";
+            }
+        }
+        $where = $where ? "WHERE ".implode(" AND ", $where) : "";
+        $sort = $sort ? "ORDER BY $sort" : "";
+        $distinct = $distinct ? "DISTINCT" : "";
+        $sql = "SELECT $distinct $fields
+                  FROM $table
+                 $where
+                  $sort";
+        return $sql;
+    }
+
+    public function db_get_sql_like($table2, array $conditions, array $fields, $distinct = false, $sort = "") {
+        $fields = $fields ? implode(',', $fields) : "*";
+        $where = array();
+        if ($conditions) {
+            foreach ($conditions as $key => $value) {
+                $value = $this->db_encode($this->db_addslashes($value));
+
+                $where[] = "$key LIKE '%$value%'";
+            }
+        }
+        $where = $where ? "WHERE ".implode(" AND ", $where) : "";
+        $sort = $sort ? "ORDER BY $sort" : "";
+        $distinct = $distinct ? "DISTINCT" : "";
+        $sql2 = "SELECT $distinct $fields
+                  FROM $table2
+                 $where
+                  $sort";
+        return $sql2;
+    }
+
+
+    /**
+     * Returns plugin config value
+     * @param  string $name
+     * @param  string $default value if config does not exist yet
+     * @return string value or default
+     */
+    public function get_config($name, $default = null) {
+        $this->load_config();
+        return isset($this->config->$name) ? $this->config->$name : $default;
+    }
+
+    /**
+     * Sets plugin config value
+     * @param  string $name name of config
+     * @param  string $value string config value, null means delete
+     * @return string value
+     */
+    public function set_config($name, $value) {
+        $pluginname = $this->get_name();
+        $this->load_config();
+        if ($value === null) {
+            unset($this->config->$name);
+        } else {
+            $this->config->$name = $value;
+        }
+        set_config($name, $value, "local_$pluginname");
+    }
+
+    /**
+     * Makes sure config is loaded and cached.
+     * @return void
+     */
+    public function load_config() {
+        if (!isset($this->config)) {
+            $name = $this->get_name();
+            $this->config = get_config("local_$name");
+        }
+    }
+}
---
+++ settings.php
@@ -0,0 +1,130 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Category creation local plugin settings and presets.
+ *
+ * @package    local_categorycreate
+ * @copyright  2017 RMOelmann
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+if ($hassiteconfig) {
+    $settings = new admin_settingpage('local_categorycreate',
+        get_string('pluginname', 'local_categorycreate'));
+    $ADMIN->add('localplugins', $settings);
+
+        // Headings.
+    $settings->add(new admin_setting_heading('local_categorycreate_settings', '',
+        get_string('pluginname_desc', 'local_categorycreate')));
+    $settings->add(new admin_setting_heading('local_categorycreate_exdbheader',
+        get_string('settingsheaderdb', 'local_categorycreate'), ''));
+
+    // Db Connection Settings.
+    // -----------------------
+
+    // Db type.
+    $options = array('',
+        "access",
+        "ado_access",
+        "ado",
+        "ado_mssql",
+        "borland_ibase",
+        "csv",
+        "db2",
+        "fbsql",
+        "firebird",
+        "ibase",
+        "informix72",
+        "informix",
+        "mssql",
+        "mssql_n",
+        "mssqlnative",
+        "mysql",
+        "mysqli",
+        "mysqlt",
+        "oci805",
+        "oci8",
+        "oci8po",
+        "odbc",
+        "odbc_mssql",
+        "odbc_oracle",
+        "oracle",
+        "pdo",
+        "postgres64",
+        "postgres7",
+        "postgres",
+        "proxy",
+        "sqlanywhere",
+        "sybase",
+        "vfp");
+    $options = array_combine($options, $options);
+    $settings->add(new admin_setting_configselect('local_categorycreate/dbtype',
+        get_string('dbtype', 'local_categorycreate'),
+        get_string('dbtype_desc', 'local_categorycreate'), '', $options));
+
+    // Db host.
+    $settings->add(new admin_setting_configtext('local_categorycreate/dbhost',
+        get_string('dbhost', 'local_categorycreate'),
+        get_string('dbhost_desc', 'local_categorycreate'), 'localhost'));
+
+    // Db User.
+    $settings->add(new admin_setting_configtext('local_categorycreate/dbuser',
+        get_string('dbuser', 'local_categorycreate'), '', ''));
+
+    // Db Password.
+    $settings->add(new admin_setting_configpasswordunmask('local_categorycreate/dbpass',
+        get_string('dbpass', 'local_categorycreate'), '', ''));
+
+    // Db Name.
+    $settings->add(new admin_setting_configtext('local_categorycreate/dbname',
+        get_string('dbname', 'local_categorycreate'),
+        get_string('dbname_desc', 'local_categorycreate'), ''));
+
+    // Db Encoding.
+    $settings->add(new admin_setting_configtext('local_categorycreate/dbencoding',
+        get_string('dbencoding', 'local_categorycreate'), '', 'utf-8'));
+
+    // Db Setup.
+    $settings->add(new admin_setting_configtext('local_categorycreate/dbsetupsql',
+        get_string('dbsetupsql', 'local_categorycreate'),
+        get_string('dbsetupsql_desc', 'local_categorycreate'), ''));
+
+    // Db Sybase.
+    $settings->add(new admin_setting_configcheckbox('local_categorycreate/dbsybasequoting',
+        get_string('dbsybasequoting', 'local_categorycreate'),
+        get_string('dbsybasequoting_desc', 'local_categorycreate'), 0));
+
+    // AODBC Debug.
+    $settings->add(new admin_setting_configcheckbox('local_categorycreate/debugdb',
+        get_string('debugdb', 'local_categorycreate'),
+        get_string('debugdb_desc', 'local_categorycreate'), 0));
+
+    // Table Settings.
+    $settings->add(new admin_setting_heading('local_categorycreate_remoteheader',
+        get_string('settingsheaderremote', 'local_categorycreate'), ''));
+
+    // Table name.
+    $settings->add(new admin_setting_configtext('local_categorycreate/remotetable1',
+        get_string('remotetable', 'local_categorycreate'),
+        get_string('remotetable_desc', 'local_categorycreate'), ''));
+    $settings->add(new admin_setting_configtext('local_categorycreate/remotetable2',
+        get_string('remotetable', 'local_categorycreate'),
+        get_string('remotetable_desc', 'local_categorycreate'), ''));
+
+}
